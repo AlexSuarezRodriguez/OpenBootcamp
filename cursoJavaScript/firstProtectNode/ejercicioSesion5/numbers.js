45df081a284b7450a5f8319d61865badf5461fe9
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-06-08 08:58:40 -0500

    ejercicio 5 javascript y node

---
+++ cursoJavaScript/firstProtectNode/ejercicioSesion5/numbers.js
@@ -0,0 +1,19 @@
+/*
+Crea un archivo JS que contenga las siguientes líneas
+- Una variable que contenga tu altura en centímetros (entero)
+- Una variable que contenga tu altura en metros (número de coma flotante)
+- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+- Una variable que contenga tu altura en metros redondeada hacia arriba
+- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+*/
+
+const heightCM = 175;
+const heightMT = 1.75;
+const weightKG = 62.3;
+const roundHeightMT = Math.ceil(heightMT);
+  console.log(roundHeightMT);
+const roundweightKG = Math.floor(weightKG);
+  console.log(roundweightKG);
+const maxValue = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+  console.log(maxValue);
