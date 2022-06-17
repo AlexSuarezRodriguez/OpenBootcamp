9b4b3fae85718ea9b4ef4e620d11f8e15ad7e6b7
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-06-17 13:20:13 -0500

    ejercicio 10 node y javascript

---
+++ cursoJavaScript/firstProtectNode/ejercicioSesion10/ejercicio/controller.js
@@ -0,0 +1,6 @@
+export function suma(num1,num2){
+  return num1+ num2;
+}
+export function multiplica(num1,num2){
+  return num1 * num2;
+}
---
+++ cursoJavaScript/firstProtectNode/ejercicioSesion10/ejercicio/index.js
@@ -0,0 +1,4 @@
+import { suma, multiplica } from "./controller.js";
+import chalk from "chalk";
+console.log(chalk.green(suma(4, 7)));
+console.log(chalk.multiplica(suma(4, 7)));
