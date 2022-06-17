0daaf7c80994f9fd6ddf24caf209b9d6e5d580ae
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-06-17 13:22:24 -0500

    ejercicio 10 node y javascript

---
+++ cursoJavaScript/firstProtectNode/ejercicioSesion10/video-librerias/index.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+  .then(function (response){
+    console.log('Success!!!')
+    console.log(response.data)
+  })
+  .catch(function (error){
+    console.log("Error")
+    console.log(error)
+  })
